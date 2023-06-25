a45267c497198d2b2b08db50c530b05d00d7ad76
calvinadhikang <calvinadhikang@gmail.com>
2023-06-26 00:10:22 +0700

    Barang Migration

---
+++ database/migrations/2023_06_25_160117_create_barang_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->increment('id');
+            $table->string('part');
+            $table->string('nama');
+            $table->string('harga');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+};
