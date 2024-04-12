70a8c232cfcf815972b85a3b4fdf6a4230271d50
calvinadhikang <calvinadhikang@gmail.com>
2024-04-13 02:02:48 +0700

    add cart

---
+++ database/migrations/2024_04_13_011145_create_cart_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->string('part');
+            $table->enum('type', ['barang', 'paket']);
+            $table->integer('qty');
+            $table->integer('subtotal');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('customer_id')->references('id')->on('customer')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+};
